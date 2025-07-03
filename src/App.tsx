8ecfac925bb55cf68e87c1b1bb43df515300235a
AgilistTim <122605124+AgilistTim@users.noreply.github.com>
2025-07-03 10:20:19 +0100

    Remove Test Admin Login button from the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { AuthProvider } from './context/AuthContext';
-import TestLoginButton from './components/TestLoginButton';
 import './index.css';
 
 function App() {
   return (
     <AuthProvider>
       <RouterProvider router={router} />
-      <TestLoginButton />
     </AuthProvider>
   );
 }
